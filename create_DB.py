0c111ac173062ccf055b6c8b465d500db175bda0
Aman Singh <amanscode2005@gmail.com>
2026-06-18 17:40:19 +0530

    initialize RAG  create db (vectorDB) and document loader

---
+++ create_DB.py
@@ -0,0 +1,30 @@
+from langchain_community.document_loaders import PyPDFLoader
+# pyrefly: ignore [missing-import]
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+loader = PyPDFLoader(
+    "documents_loaders/Under-Discussed Topic_ AI Model Collapse due to Synthetic Data.pdf"
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = text_splitter.split_documents(docs)
+
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding,
+    persist_directory="chromaDB"
+)
